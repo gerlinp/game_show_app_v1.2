67a7b898f23678ca894bfac57e8540744d706d44
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-11 03:36:52 -0500

    More Variables needed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const reset = document.querySelector('.btn__reset');
-
+const overlay = document.querySelector('#overlay');
+let miss = '';
 
 
 
