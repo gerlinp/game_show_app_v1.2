5d61b431d8b3d5ea6d3f213ec7782ad231ca5439
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-11 03:37:06 -0500

    Phrases

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const phrase = document.querySelector('#phrase');
 const reset = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
 let miss = '';
+let phrases = [
+    'May the Force be with you',
+    'Theres no place like home',
+    'Break a leg',
+    'Just keep swimming',
+    'To infinity and beyond' 
+];
+
 
 
 
