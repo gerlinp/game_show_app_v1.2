209ab29699f61513b881f579536dd37ff0b36fd1
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-15 18:41:18 -0400

    addPhraseToDisplay completed

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const reset = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
 let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let miss = '';
+let answer = [];
+let ul = document.querySelector('ul');
 let phrases = [
     'May the Force be with you',
     'Theres no place like home',
@@ -17,34 +19,32 @@ reset.addEventListener('click', () => {
 
 function getRandomPhraseAsArray(arr) { // Selecting a random phrase
     let random = Math.floor(Math.random() * arr.length);
-    return arr[random].split('');
+    answer = arr[random].split('');
+    return answer
 };
-
+getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay() { // Set random phrase chose to be displayed
-    for (i = 0; i < getRandomPhraseAsArray(phrases).length; i++) {    
+    for (i = 0; i < answer.length; i++) {  
         let li = document.createElement('li');
-        li.textContent = getRandomPhraseAsArray(phrases)[i];
-
+        li.textContent = answer[i];
         if (li.textContent === ' ') {
             li.className = 'space';
+            ul.appendChild(li);
         } else {
-            li.className = 'letter';
-        }        
-        console.log(li)
+            li.className = 'letter'; 
+            ul.appendChild(li);
+        }       
     }
 };
+addPhraseToDisplay()
 
-
-// Create a checkLetter function
-// Create a function “stub” for the checkLetter function
-// Include a parameter in the function head for the button that gets clicked
 function checkLetter() {
 // Store all of the li elements in a variable inside checkLetter
 // Create a variable to store if a match is found and give it an initial value of null
     let match = [];
 // Loop through all of the li elements. Remember: arrays start with index 0!
-    for (i = 0; i < randomPhrase.length; i++) {
+    for (i = 0; i < ul.length; i++) {
         // Create a conditional that compares the text of the button parameter to the text of the li at the current index of the loop
         if (arr === randomPhrase.textContent) {
         // If they match, add the “show” class to the li
