9c1efcc1a5e43e06d33bf539b3d7798dd302ad94
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-16 04:58:00 -0400

    Met expectations

--- app.js
+++ app.js
@@ -1,63 +1,87 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
-const reset = document.querySelector('.btn__reset');
-const overlay = document.querySelector('#overlay');
-let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let miss = '';
-let answer = [];
-let ul = document.querySelector('ul');
-let phrases = [
+const startButton = document.querySelector('.btn__reset');
+let miss = 0;
+let title = document.querySelector('.title')
+const phrases = [
     'May the Force be with you',
     'Theres no place like home',
     'Break a leg',
     'Just keep swimming',
     'To infinity and beyond' 
 ];
-reset.addEventListener('click', () => {
-    overlay.style.display = 'none';
-});
 
-function getRandomPhraseAsArray(arr) { // Selecting a random phrase
+// return a random phrase from an array
+const getRandomPhraseArray = arr => {
     let random = Math.floor(Math.random() * arr.length);
-    answer = arr[random].split('');
-    return answer
-};
-getRandomPhraseAsArray(phrases);
+    let answer = phrases[random].split('');
+    return answer;
+}   
+let answer = getRandomPhraseArray(phrases);
 
-function addPhraseToDisplay() { // Set random phrase chose to be displayed
-    for (i = 0; i < answer.length; i++) {  
+// adds the letters of a string to the display
+const addPhraseToDisplay =  arr => {
+    for (let i = 0; i < arr.length; i++) {
         let li = document.createElement('li');
-        li.textContent = answer[i];
-        if (li.textContent === ' ') {
-            li.className = 'space';
-            ul.appendChild(li);
-        } else {
+        li.textContent = arr[i];
+        phrase.appendChild(li);
+        if (li.textContent !== ' ') {
             li.className = 'letter'; 
-            ul.appendChild(li);
-        }       
+        } else {
+            li.className = 'space'
+        };
     }
 };
-addPhraseToDisplay()
+addPhraseToDisplay(answer);
 
-function checkLetter() {
-// Store all of the li elements in a variable inside checkLetter
-// Create a variable to store if a match is found and give it an initial value of null
-    let match = [];
-// Loop through all of the li elements. Remember: arrays start with index 0!
-    for (i = 0; i < ul.length; i++) {
-        // Create a conditional that compares the text of the button parameter to the text of the li at the current index of the loop
-        if (arr === randomPhrase.textContent) {
-        // If they match, add the “show” class to the li
-            randomPhrase.className = show    
-        // If they match, store the button text in the match variable
-            match.push(randomPhrase.textContent);
+// check if a ltter is in the phrase
+const checkLetter = button => {
+    let lis = document.getElementById("phrase").getElementsByTagName("li");
+    let match;
+    for (let i = 0; i < lis.length; i++) {
+        if (button.textContent === lis[i].textContent.toLowerCase() ) {
+            lis[i].classList.add('show')
+            match = button;
         }
-    }
+    } 
     return match;
+    
 }
 
+// check if the game has been won or lost
+const checkWin = () => {
+    let letter = document.querySelectorAll('.letter');
+    console.log(letter);
+    let show = document.querySelectorAll('.show');
+    console.log(show);
+    if (letter.length === show.length) {
+        overlay.classList.add('win');
+        overlay.style.display = 'flex'
+    } else if ( miss > 4 ) {
+        overlay.classList.add('lose');
+        overlay.style.display = 'flex'
+        title.innerText = 'Ran out of lives'
 
+    }
+    
+}
 
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+        overlay.style.display = 'none';
+});
 
-
-
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+    if (e.target.classList.contains('chosen')) {
+        console.log('Already chosen');
+    } else if (e.target.tagName === 'BUTTON') {
+            e.target.classList.add('chosen')
+            if (checkLetter(e.target) === undefined) {
+                let tries = document.querySelector('.tries')
+                let ol =  document.querySelector('ol')
+                ol.removeChild(tries);
+                miss++
+        }
+    } checkWin();
+});
