02ae2580a73becdac1e8d303ed305ed7506d6e08
gerlinp <gerlin.pierrelouis@gmail.com>
2021-05-24 01:59:10 -0400

    revamp

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('.btn__reset');
+const startGame = document.querySelector('.btn__reset');
+const playAgain = document.querySelector('.btn_restart');
 let miss = 0;
 let title = document.querySelector('.title')
 let displayedAnswer = ''
@@ -72,15 +74,6 @@ const checkWin = () => {
     }
 }
 
-const startGame = document.querySelector('.btn__reset');
-const playAgain = document.querySelector('.btn_restart');
-
-
-
-
-
-
-
 // listen for the start game button to be pressed
 startButton.addEventListener('click', () => {
         overlay.style.display = 'none';
@@ -100,5 +93,55 @@ qwerty.addEventListener('click', e => {
         }
     } checkWin();
 });
+
+
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#0f0';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }
+  }
+}
+
+// Loop the animation
+setInterval(draw, 33);
+
+
+
+
+
+
+
+/------------------------start----------------------/ 
+
 
 
